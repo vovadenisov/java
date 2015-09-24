7a6ac665ebb772b894393199f8c7854f24869888
vovadenisov <vovadenisov@yandex.ru>
2015-09-24 15:17:23 +0300

    new_project

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -18,7 +18,7 @@ import javax.servlet.Servlet;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        int port = 8080;
+        int port = 8400;
         if (args.length == 1) {
             String portString = args[0];
             port = Integer.valueOf(portString);

b574d02a34c82ffe7301ae8ebbbd094de1da9804
allavlasova <allavlasova93@mail.ru>
2015-10-23 13:49:02 +0300

    test

---
+++ src/main/test/main/AccountServiceTest.java
@@ -0,0 +1,74 @@
+package main;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by alla on 22.10.15.
+ */
+public class AccountServiceTest {
+
+    @Test
+    public void testCheckUser() throws Exception {
+
+    }
+
+   /* @Test
+    public void testCheckUserlogin() throws Exception {
+
+    }
+
+    @Test
+    public void testNumberOfRegistered() throws Exception {
+
+    }
+
+    @Test
+    public void testNumberOfSessions() throws Exception {
+
+    }
+
+    @Test
+    public void testAddUser() throws Exception {
+
+    }
+
+    @Test
+    public void testCheckSeassions() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveSeassions() throws Exception {
+
+    }
+
+    @Test
+    public void testAddSessions() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCurrentUser() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUser() throws Exception {
+
+    }
+
+    @Test
+    public void testUserSession() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSessions() throws Exception {
+
+    }*/
+}
